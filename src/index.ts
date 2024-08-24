3ca84c68759dfd3110a9165344b48cb82621c90c
Rilton Bispo <riltondev@gmail.com>
2024-08-24 13:09:22 -0300

    feat: render agents

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,48 @@
 
-const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+import { Agent } from './types/agents'
 
-console.log(greet("Rilton"));
+let agents: Agent[] | null = null;
+
+async function getData() {
+  const url = "https://valorant-api.com/v1/agents";
+  const response = await fetch(url);
+  const json = await response.json();
+  agents = json.data
+  agents && renderAgents(agents)
+}
+
+const renderAgents = (agents: Agent[]) => {
+  const container = document.getElementById('agent-container');
+
+  agents.forEach(agent => {
+    const agentDiv = document.createElement('div');
+    agentDiv.classList.add('agent');
+
+    const agentLink = document.createElement('a');
+    agentLink.href = `agent.html?id=${agent.uuid}`;
+    agentLink.classList.add('agent__link');
+
+    const agentImage = document.createElement('img');
+    agentImage.src = agent.displayIcon;
+    agentImage.alt = agent.displayName;
+    agentImage.classList.add('agent__image');
+
+    const agentTitle = document.createElement('h2');
+    agentTitle.classList.add('agent__title');
+    agentTitle.textContent = agent.displayName;
+
+    agentImage.addEventListener('mouseenter', () => {
+      const gradientColor = '#' + agent.backgroundGradientColors[0]
+      console.log(gradientColor)
+      document.documentElement.style.setProperty('--bg-color', gradientColor);
+    });
+
+    agentLink.appendChild(agentImage);
+    agentLink.appendChild(agentTitle);
+    agentDiv.appendChild(agentLink);
+    container?.appendChild(agentDiv);
+  })
+}
+
+
+getData()
