26f45830b974a0789f0d0555dce31681dcd15db5
Rilton Bispo <riltondev@gmail.com>
2024-09-11 20:30:15 -0300

    refactor: render agents with inner html

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { Agent } from './types/agents'
 
 let agents: Agent[] | null = null;
@@ -7,41 +6,43 @@ async function getData() {
   const url = "https://valorant-api.com/v1/agents";
   const response = await fetch(url);
   const json = await response.json();
-  agents = json.data
-  agents && renderAgents(agents)
+  agents = json.data;
+  agents && renderAgents(agents);
 }
 
 const renderAgents = (agents: Agent[]) => {
   const container = document.getElementById('agent-container');
 
-  agents.forEach(agent => {
-    const agentDiv = document.createElement('div');
-    agentDiv.classList.add('agent');
+  if (container) {
+    let agentsHTML = '';
 
-    const agentLink = document.createElement('a');
-    agentLink.href = `agent.html?id=${agent.uuid}`;
-    agentLink.classList.add('agent__link');
-
-    const agentImage = document.createElement('img');
-    agentImage.src = agent.displayIcon;
-    agentImage.alt = agent.displayName;
-    agentImage.classList.add('agent__image');
-
-    const agentTitle = document.createElement('h2');
-    agentTitle.classList.add('agent__title');
-    agentTitle.textContent = agent.displayName;
-
-    agentImage.addEventListener('mouseenter', () => {
-      const gradientColor = '#' + agent.backgroundGradientColors[0]
-      document.documentElement.style.setProperty('--bg-color', gradientColor);
+    agents.forEach(agent => {
+      agentsHTML += `
+        <div class="agent">
+          <a href="agent.html?id=${agent.uuid}" class="agent__link">
+            <img 
+              src="${agent.displayIcon}" 
+              alt="${agent.displayName}" 
+              class="agent__image" 
+              data-bgcolor="${agent.backgroundGradientColors[0]}"
+            />
+            <h2 class="agent__title">${agent.displayName}</h2>
+          </a>
+        </div>
+      `;
     });
 
-    agentLink.appendChild(agentImage);
-    agentLink.appendChild(agentTitle);
-    agentDiv.appendChild(agentLink);
-    container?.appendChild(agentDiv);
-  })
+    container.innerHTML = agentsHTML;
+
+    const agentImages = container.querySelectorAll('.agent__image')
+    
+    agentImages.forEach(image => {
+      image.addEventListener('mouseenter', () => {
+        const gradientColor = '#' + image.getAttribute('data-bgcolor');
+        document.documentElement.style.setProperty('--bg-color', gradientColor);
+      });
+    });
+  }
 }
 
-
-getData()
+getData();
